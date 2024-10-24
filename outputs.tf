9d0c94e0bfa7196e22b9d0f24ea94f6bf10ad371
siri <o1sridevi20@gmail.com>
2024-10-24 17:30:08 +0530

    addfilesforstatelocktesting

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "valueofinstanceid" {
+    value = aws_instance.example2.id
+  
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "ami" {
+   description = "value"
+
+  
+}
+variable "instance_type" {
+    description = "value"
+  
+}
+variable "region" {
+    description = "value"
+  
+}
